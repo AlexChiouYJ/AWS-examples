19a69ca558c2679b2f978f5660d48b4eb9b31a64
AlexChiouYJ c <alexchiou010622@gmail.com>
2025-06-04 07:53:05 +0000

    try

--- s3/iac/cdk/lib/cdk-stack.ts
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -1,11 +1,10 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 
 export class CdkStack extends Stack {
   constructor(scope: cdk.App, id: string, props?: StackProps) {
     super(scope, id, props);
-    //const bucket = new s3.Bucket(scope, 'MyBucket');
+    const bucket = new s3.Bucket(this, 'MyBucket');
   }
 }

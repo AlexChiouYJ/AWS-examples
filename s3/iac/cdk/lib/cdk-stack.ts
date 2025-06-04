502fdde1fde2a336057203798158695474db0d99
AlexChiouYJ c <alexchiou010622@gmail.com>
2025-06-04 04:20:29 +0000

    add git from CDK in stack.ts

--- s3/iac/cdk/lib/cdk-stack.ts
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -1,9 +1,10 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 
 export class CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: cdk.App, id: string, props?: StackProps) {
     super(scope, id, props);
     //const bucket = new s3.Bucket(scope, 'MyBucket');
   }

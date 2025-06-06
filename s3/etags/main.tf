175ee38a4563fd2da8e0c01103335051d27ba5dd
AlexChiouYJ c <alexchiou010622@gmail.com>
2025-06-06 10:33:36 +0000

    refactor etag

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -7,11 +7,20 @@ terraform {
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 resource "aws_s3_bucket" "default" {
 }
 
-resource "aws_s3_bucket_object" "object" {
-  bucket = resource aws_s3_bucket default
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
   key    = "myfile.txt"
   source = "myfile.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("myfile.txt")
 }
